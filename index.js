cfda1fd4dbd894264e0bce1624af5292e188bc16
philipwanjiku <muhorophilip7@gmail.com>
2023-10-14 10:01:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+
+
+};
+
+function setBestCustomer (){
+
+    bestCustomer = 'not bob' ;
+
+};
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
+};
+
+const leastfavoritecustomer = 'Theo'
+
+ const leastfavoritecustomer2 = leastfavoritecustomer;
+ function changeLeastFavoriteCustomer(){
+ leastfavoritecustomer2 = "Marcus"
+};
+  
+  
+  
+  
+  
